b7e0227d06bae626c24ed8bb55e0e025edd7f24a
Samuel Webster <samuel.webster515@gmail.com>
2024-02-19 21:26:11 +0000

    init

---
+++ index.ts
@@ -0,0 +1,12 @@
+/**
+ * * The HelloWorld program implements an application that
+ * * simply displays "Hello World!" to the standard output.
+ * *
+ * * By:      Samuel Webster
+ * * Version: 1.0
+ * * Since:   2024-01-01
+ * */ 
+
+console.log("Hello World!")
+
+console.log("\nDone.")
